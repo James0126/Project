cdba14f43acfd800a525c1cbc65249e4450971af
James <wjm0126@naver.com>
2020-10-07 17:05:14 +0900

    add auto type

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -3,46 +3,45 @@
 using namespace std;
 
 
-void QSort(vector<int>& vec, int left, int right)
+void QuickSort(vector<auto>& vec, int left, int right)
 {
     int pl = left;
     int pr = right;
     
-    int center = vec[(pl + pr) / 2];
+    auto center = vec[(pl+pr)/2];
     
     while(pl<=pr)
     {
-        while(vec[pl] < center)
+        while(vec[pl]<center)
             pl++;
-        
-        while(vec[pr] > center)
+        while(vec[pr]>center)
             pr--;
             
         if(pl<=pr)
         {
-            int dest = vec[pl];
+            auto dest = vec[pl];
             vec[pl] = vec[pr];
             vec[pr] = dest;
+            
             pl++;
             pr--;
         }
-        
-        if(left < pr) QSort(vec, left, pr);
-        if(pl < right) QSort(vec, pl, right);
-    }   
+    }
+    
+    if(left<pr)QuickSort(vec, left, pr);
+    if(right>pl)QuickSort(vec, pl, right);
 }
 
 int main()
 {
-   vector<int> d{5,3,2,6,7,9,1,8,2};
-   
-   QSort(d, 0, d.size()-1);
-   
+    vector<char> d{'a','c','w','y','q'};
+
+    QuickSort(d, 0, d.size()-1);
+    
     for(int i =0; i < d.size(); i++)
     {
         cout<<d[i]<<" ";
     }
 
-   
-   return 0;
+    return 0;
 }
